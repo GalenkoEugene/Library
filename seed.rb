7a40de5b8687e5349d6cdf850f283e09ad4bb855
Eugen <Re4port@ukr.net>
2017-04-19 13:03:31 +0300

    seed unique fields
    add unique authors and titles for Books
    and unique email for Readers

--- seed.rb
+++ seed.rb
@@ -2,15 +2,15 @@ require './classes/library'
 
 def seed_db
   authors = []
-  7.times { authors << Author.new(Faker::Book.author, 
+  7.times { authors << Author.new(Faker::Book.unique.author, 
 								Faker::Lorem.paragraph) }
   books = []
-  22.times{ books << Book.new(Faker::Book.title,  
+  22.times{ books << Book.new(Faker::Book.unique.title,  
 							authors.sample) }
 
   readers = []  
   6.times{ readers << Reader.new(Faker::Name.first_name,
-								Faker::Internet.email,
+								Faker::Internet.unique.email,
 								Faker::Address.city,
 								Faker::Address.street_name,
 								Faker::Address.building_number) }
